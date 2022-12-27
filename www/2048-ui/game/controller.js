30575b8cf6499dcd0c38d48eb9ee830981859e9e
Light <coolkevin011@gmail.com>
2022-12-27 20:45:55 +0530

    add event listeners for arrow keys

---
+++ www/2048-ui/game/controller.js
@@ -0,0 +1,30 @@
+import { Direction } from "2048-rs";
+
+const UP_KEY = 'ArrowUp';
+const DOWN_KEY = 'ArrowDown';
+const LEFT_KEY = 'ArrowLeft';
+const RIGHT_KEY = 'ArrowRight';
+
+export function addEventListeners(game) {
+    document.addEventListener('keyup', function (e) {
+        const code = e.code;
+        switch (code) {
+            case UP_KEY: {
+                game.play_swipe(Direction.Up);
+                break;
+            }
+            case LEFT_KEY: {
+                game.play_swipe(Direction.Left);
+                break;
+            }
+            case RIGHT_KEY: {
+                game.play_swipe(Direction.Right);
+                break;
+            }
+            case DOWN_KEY: {
+                game.play_swipe(Direction.Right);
+                break;
+            }
+        }
+    });
+}
--- www/2048-ui/index.js
+++ www/2048-ui/index.js
@@ -1,11 +1,14 @@
 
 import { Game } from "2048-rs";
+import { addEventListeners } from "./game/controller";
 import { GameRenderer } from "./game/render-game";
 
 const canvas = document.getElementById("game-canvas");
 let game = Game.new();
 
 let renderer = new GameRenderer(canvas, game);
+
+addEventListeners(game);
 
 const renderLoop = () => {
     renderer.drawGrid();
