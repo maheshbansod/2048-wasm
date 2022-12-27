f66d237991225a470350f3652aeda18000e91a7a
Light <coolkevin011@gmail.com>
2022-12-27 21:46:04 +0530

    feat: ignore illegal move errors

--- www/2048-ui/game/controller.js
+++ www/2048-ui/game/controller.js
@@ -8,22 +8,30 @@ const RIGHT_KEY = 'ArrowRight';
 export function addEventListeners(game) {
     document.addEventListener('keyup', function (e) {
         const code = e.code;
-        switch (code) {
-            case UP_KEY: {
-                game.play_swipe(Direction.Up);
-                break;
+        try {
+            switch (code) {
+                case UP_KEY: {
+                    game.play_swipe(Direction.Up);
+                    break;
+                }
+                case LEFT_KEY: {
+                    game.play_swipe(Direction.Left);
+                    break;
+                }
+                case RIGHT_KEY: {
+                    game.play_swipe(Direction.Right);
+                    break;
+                }
+                case DOWN_KEY: {
+                    game.play_swipe(Direction.Right);
+                    break;
+                }
             }
-            case LEFT_KEY: {
-                game.play_swipe(Direction.Left);
-                break;
-            }
-            case RIGHT_KEY: {
-                game.play_swipe(Direction.Right);
-                break;
-            }
-            case DOWN_KEY: {
-                game.play_swipe(Direction.Right);
-                break;
+        } catch (e) {
+            if (e === "Illegal move") {
+                // illegal move
+            } else {
+                console.error(e);
             }
         }
     });
